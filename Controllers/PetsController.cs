1e1417baf7bfafa1116b74f22f3150ea03b49542
CodyBanks <codydouglasbanks@gmail.com>
2020-06-05 15:33:27 -0400

    Added feature to view all pets, and delete a pet

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -18,5 +18,43 @@ namespace PetDatabaseAPI.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pet>> GetAll()
+        {
+            var allThePets = _context.Pets;
+
+            return Ok(allThePets);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Pet> GetByID(int id)
+        {
+            var pet = _context.Pets.FirstOrDefault(pet => pet.Id == id);
+
+            if (pet == null)
+            {
+                // Give back a 404 error
+                return NotFound();
+            }
+
+            return Ok(pet);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Pet> Delete(int id)
+        {
+            var foundPet = _context.Pets.FirstOrDefault(pet => pet.Id == id);
+
+            if (foundPet == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pets.Remove(foundPet);
+            _context.SaveChanges();
+
+            return Ok(foundPet);
+        }
     }
 }
